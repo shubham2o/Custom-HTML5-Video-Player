3e77c96970e543d2aae5535c5a101f01b87a8549
shubham2o <shubhamsharma.ha@gmail.com>
2022-04-26 07:38:35 +0530

    HTML Audio/Video DOM pause() Method added

--- Doubts/doubts.js
+++ Doubts/doubts.js
@@ -13,3 +13,16 @@ function myFunction2() {
     var a = document.getElementById("myVideo2");
     document.getElementById("demo2").innerHTML = "Start : " + a.played.start(0) + " End : " + a.played.end(0);
 }
+
+
+// HTML Audio/Video DOM pause() Method
+// EXAMPLE
+var vid3 = document.getElementById("myVideo3");
+
+function playVid() {
+    vid3.play();
+}
+
+function pauseVid() {
+    vid3.pause();
+}
