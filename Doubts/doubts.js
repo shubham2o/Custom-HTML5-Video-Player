a68adacc46b06d3f86fc6e8944ceed1ddff7b906
shubham2o <shubhamsharma.ha@gmail.com>
2022-04-26 07:26:56 +0530

    Video played Property added

--- Doubts/doubts.js
+++ Doubts/doubts.js
@@ -5,3 +5,11 @@ var vid = document.getElementById("myVideo1");
 function myFunction1() {
     alert(vid.paused);
 }
+
+
+// Video played Property
+// EXAMPLE
+function myFunction2() {
+    var a = document.getElementById("myVideo2");
+    document.getElementById("demo2").innerHTML = "Start : " + a.played.start(0) + " End : " + a.played.end(0);
+}
