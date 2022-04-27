108bba8b3f83feda72420d4da105597db8801c9b
shubham2o <shubhamsharma.ha@gmail.com>
2022-04-27 19:06:43 +0530

    script . js bugs ressolved and added

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Get our elements
+/* Get Our Elements */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('toggle');
-const skipButton = player.querySelectorAll('[data-skip]');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 
-// Build out functions
+/* Build out functions */
 function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
@@ -44,7 +44,7 @@ function scrub(e) {
 }
 
 
-// Hook up the event listeners
+/* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
